3c23480c81cc54d14de5696e2977a44a9e66d386
je-swy <kateryna.shemonaieva@gmail.com>
2025-09-12 13:45:40 +0300

    Challenge 4: Done

--- 4 - Modifying DOM elements/Challenge/Challenge/script.js
+++ 4 - Modifying DOM elements/Challenge/Challenge/script.js
@@ -4,4 +4,11 @@ function changeStyle() {
     // 2. Change the font size of 'dynamicText' to '18px'.
     // 3. Toggle the font weight of 'dynamicText' between 'normal' and 'bold' on each button click.
     // 4. Change the text color of 'dynamicText' to 'green'.
+
+    let dynamicText = document.getElementById('dynamicText');
+    dynamicText.style.backgroundColor = 'lightblue';
+    dynamicText.style.fontSize = '18px';
+    dynamicText.style.fontWeight = dynamicText.style.fontWeight === "bold" ? "normal" : "bold";
+    dynamicText.style.color = 'green';
 }
+
