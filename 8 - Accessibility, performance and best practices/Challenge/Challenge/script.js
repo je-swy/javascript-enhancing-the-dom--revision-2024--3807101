1ff4270b9c0f37e95d93fb420b1129a1f8693e7c
je-swy <kateryna.shemonaieva@gmail.com>
2025-09-15 12:34:35 +0300

    Challenge 8: Done

--- 8 - Accessibility, performance and best practices/Challenge/Challenge/script.js
+++ 8 - Accessibility, performance and best practices/Challenge/Challenge/script.js
@@ -4,3 +4,9 @@
 // 3. Implement changes to enhance accessibility.
 // 4. Test the updated page using tools like screen readers or accessibility checkers.
 
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+        alert('Button Clicked!');
+    });
+});
